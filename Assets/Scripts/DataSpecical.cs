72e0b3c5e390d42bc1e5362b6f1dc9353828bd5c
LyTvest <bdm1999@yandex.ru>
2021-06-18 22:42:23 +0500

    http

---
+++ Assets/Scripts/DataSpecical.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataSpecical 
+{
+    string NameSpecial = "";
+    string FirstName = "";
+    string LastName = "";
+    string Phone = "";
+}
---
+++ Assets/Scripts/ProfileSetting.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProfileSetting : MonoBehaviour
+{
+    string FirstName = "";
+    string LastName = "";
+    string Phone = ""; 
+}
