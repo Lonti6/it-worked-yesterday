ae3c38ca249011ce0cae13319ee284c88c70a1a5
Lonti6 <72629247+Lonti6@users.noreply.github.com>
2021-06-19 18:47:04 +0500

    р

---
+++ Assets/Scripts/CheckProfileLoaded.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace DefaultNamespace
+{
+    public class CheckProfileLoaded : MonoBehaviour
+    {
+        private void Start()
+        {
+            ProfileSetting.loadProfile();
+            if (ProfileSetting.PROFILE.FirstName.Length > 0)
+            {
+                SceneManager.LoadScene("Main");
+            }
+        }
+    }
+}
